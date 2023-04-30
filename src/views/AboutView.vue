a775c38105816a53f1217659781e66a6dec65db4
Monika Uprety <100415902+monikauprety@users.noreply.github.com>
2023-04-30 10:33:28 +0545

    initial

---
+++ src/views/AboutView.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="text-center">
+        <h2 class="text-center text-xl sm:text-2xl font-bold uppercase dark:text-white">Hi! it's Monika Uprety </h2>
+        <p class="sm:text-center text-justify my-4 sm:w-4/12 mx-auto dark:text-gray-300">
+            Passionate about app development, I'm expanding my knowledge by mastering
+            Flutter. With a keen
+            eye for detail and a commitment to excellence, I create captivating and user-friendly mobile applications. Let's
+            build something amazing together!</p>
+        <p class="text-center text-blue-600">#Flutter #AppDevelopment</p>
+    </div>
+</template>
+<script setup>
+
+
+</script>
